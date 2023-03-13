9ac02cd911ee37bf49d2facfb83cabf285d18dcb
Brian Faust <hello@basecode.sh>
2023-03-13 07:49:30 +0200

    refactor: make use of automatic service provider configuration

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace PreemStudio\Interfail;
 
 use PreemStudio\Jetpack\Package\AbstractServiceProvider;
-use PreemStudio\Jetpack\Package\Package;
 
 final class ServiceProvider extends AbstractServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        $package->name('laravel-interfail');
-    }
+    //
 }
